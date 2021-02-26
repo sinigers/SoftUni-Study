3070c6134c9f9c7010ea32c133db1ad49af3500e
sinigers <sinigers@yahoo.com>
2021-02-26 08:49:23 +0200

    fixes

--- Js-Advanced/10. Task Manager/app.js
+++ Js-Advanced/10. Task Manager/app.js
@@ -1,81 +1,72 @@
 function solve() {
-
-let taskInpt=document.querySelector("#task");
-let taskDescr=document.querySelector("#description");
-let taskDate=document.querySelector("#date");
+  let taskInpt = document.querySelector("#task");
+  let taskDescr = document.querySelector("#description");
+  let taskDate = document.querySelector("#date");
 
   document.querySelector("body > main").addEventListener("click", addItem);
   function addItem(ev) {
-    
-     event.preventDefault(); //prevent refreshing
-     // if empty inputs do nothing
-    if (document.querySelector("#task").value == "" || document.querySelector("#description").value == "" || document.querySelector("#date").value == "") {
-  return
-  //  return alert("Input must be filled");
-  // throw Error("Input must be filled");
-   } else if (ev.target.textContent === "Add") {
-     
+    event.preventDefault(); //prevent refreshing
 
-      // create h3, text cont -sdf.., classname btn TEMPLATE
-      // let newHfromFunction= createElements("h3", "sdfsdf", "btn");
-      let newArticle = createEl("article", "", "");
-      let newH = createEl("h3", taskInpt.value, "");
-      newArticle.appendChild(newH);
-      taskInpt.value="";
+    if (ev.target.textContent === "Add") {
+      // if empty inputs do nothing
+      if (
+        taskInpt.value == "" ||
+        taskDescr.value == "" ||
+        taskDate.value == ""
+      ) {
+        return;
+        //  return alert("Input must be filled");
+        // throw Error("Input must be filled");
+      } else {
+        // create h3, text cont -sdf.., classname btn TEMPLATE
+        // let newHfromFunction= createElements("h3", "sdfsdf", "btn");
+        let newArticle = createEl("article", "", "");
+        let newH = createEl("h3", taskInpt.value, "");
+        newArticle.appendChild(newH);
+        taskInpt.value = "";
 
-      let newPdescr = createEl(
-        "p",
-        `Description: ${taskDescr.value}`,
-        ""
-      );
-      newArticle.appendChild(newPdescr);
-      taskDescr.value="";
+        let newPdescr = createEl("p", `Description: ${taskDescr.value}`, "");
+        newArticle.appendChild(newPdescr);
+        taskDescr.value = "";
 
-      let newPdate = createEl(
-        "p",
-        `Due date: ${taskDate.value}`,
-        ""
-      );
-      newArticle.appendChild(newPdate);
-      taskDate.value="";
+        let newPdate = createEl("p", `Due date: ${taskDate.value}`, "");
+        newArticle.appendChild(newPdate);
+        taskDate.value = "";
 
-      let newDiv = createEl("div", "", "flex");
+        let newDiv = createEl("div", "", "flex");
 
-      let newBdelete = createEl("button", "Delete", "red");
-      newDiv.appendChild(newBdelete);
+        let newBdelete = createEl("button", "Delete", "red");
+        newDiv.appendChild(newBdelete);
 
-      let newBstart = createEl("button", "Start", "green");
-      newDiv.appendChild(newBstart);
+        let newBstart = createEl("button", "Start", "green");
+        newDiv.appendChild(newBstart);
 
-      newArticle.appendChild(newDiv);
+        newArticle.appendChild(newDiv);
 
-      document
-        .querySelector(
-          "body > main > div > section:nth-child(2) > div:nth-child(2)"
-        )
-        .appendChild(newArticle);
-       
-      
-    }else if (ev.target.textContent === "Delete") {
-      let targetArticle= ev.target.parentNode.parentNode;    
-      targetArticle.parentNode.removeChild(targetArticle)
+        document
+          .querySelector(
+            "body > main > div > section:nth-child(2) > div:nth-child(2)"
+          )
+          .appendChild(newArticle);
+      }
+    } else if (ev.target.textContent === "Delete") {
+      console.log("sdfsfds");
+      let targetArticle = ev.target.parentNode.parentNode;
+      targetArticle.parentNode.removeChild(targetArticle);
+    } else if (ev.target.textContent === "Start") {
+      let targetForProgress = ev.target.parentNode.parentNode;
+      document.querySelector("#in-progress").appendChild(targetForProgress);
+      ev.target.textContent = "Finish";
+    } else if (ev.target.textContent === "Finish") {
+      let targetFinish = ev.target.parentNode.parentNode;
+      document.querySelector("#compl").appendChild(targetFinish);
+      let divFordel = ev.target.parentNode;
 
-    }else if (ev.target.textContent === "Start") {
-      let targetForProgress= ev.target.parentNode.parentNode 
-      document.querySelector("#in-progress").appendChild(targetForProgress)
-        ev.target.textContent="Finish"
-        
-    }else if (ev.target.textContent === "Finish") {
-    let targetFinish= ev.target.parentNode.parentNode; 
-    document.querySelector("#compl").appendChild(targetFinish)
-  let divFordel= ev.target.parentNode;
-
-    // remove buttons
-    divFordel.parentNode.removeChild(divFordel)
-    
+      // remove buttons
+      divFordel.parentNode.removeChild(divFordel);
+    }
   }
-    
-  }
+
   //in progress
 
   // create element function
